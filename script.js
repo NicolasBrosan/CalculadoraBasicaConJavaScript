5482c48b20c1ad9617020f2aea50256b9aae7409
Nicolas Brosan <88517335+NicolasBrosan@users.noreply.github.com>
2023-08-21 20:19:57 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+//Esto agregar a la pantalla el valor del boton de la calculadora que toquemos
+function agregar(valor){
+    document.getElementById('pantalla').value += valor
+}
+
+//Esta accion vacia la pantalla
+function borrar(){
+    document.getElementById('pantalla').value = ''
+}
+
+//Esta accion toma el valor de la pantalla y los  ejecuta los calculos
+function calcular(){
+    const valorPantalla = document.getElementById('pantalla').value
+    const resultado = eval(valorPantalla)
+    document.getElementById('pantalla').value = resultado
+}
